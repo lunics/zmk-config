e14eb15ac8b32aa5e81f202f9a14f67da5233491
lunics <git.dev@lunics.fr>
2023-07-28 21:42:54 +0200

    add layout numbers

--- config/defines.h
+++ config/defines.h
@@ -45,8 +45,9 @@
 #define num_8 &kp NUMBER_8
 #define num_9 &kp NUMBER_9
 
-#define l_def  0    // layout default
-#define l_num  1    // layout numbers
+#define l_def  0      // layout default
+#define l_num  1      // layout numbers
+#define sl_num &sl 1  // sticky layout numbers
 #define l_sym  2
 #define l_nav  3
 #define l_lmod 4
@@ -54,7 +55,6 @@
 #define l_fun  7
 
 #define ko_ &none
-
 
 
 
