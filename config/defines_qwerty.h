e686bc90cc1e1184fffa6487a09573849f394fd5
lunics <git.dev@lunics.fr>
2023-07-29 18:59:00 +0200

    add defines qwerty, fix defines numbers

---
+++ config/defines_qwerty.h
@@ -0,0 +1,45 @@
+#define a_ &kp A
+#define b_ &kp B
+#define c_ &kp C
+#define d_ &kp D
+#define e_ &kp E
+#define f_ &kp F
+#define g_ &kp G
+#define h_ &kp H
+#define i_ &kp I
+#define j_ &kp J
+#define k_ &kp K
+#define l_ &kp L
+#define m_ &kp M
+#define n_ &kp N
+#define o_ &kp O
+#define p_ &kp P
+#define q_ &kp Q
+#define r_ &kp R
+#define s_ &kp S
+#define t_ &kp T
+#define u_ &kp U
+#define v_ &kp V
+#define w_ &kp W
+#define x_ &kp X
+#define y_ &kp Y
+#define z_ &kp Z
+
+#define num_0 $kp NUMBER_0
+#define num_1 $kp NUMBER_1
+#define num_2 $kp NUMBER_2
+#define num_3 $kp NUMBER_3
+#define num_4 $kp NUMBER_4
+#define num_5 $kp NUMBER_5
+#define num_6 $kp NUMBER_6
+#define num_7 $kp NUMBER_7
+#define num_8 $kp NUMBER_8
+#define num_9 $kp NUMBER_9
+
+#define enter  &kp ENTER
+#define tab    &kp TAB
+#define space  &kp SPACE
+#define comma  &kp COMMA      // ,
+#define scolon &kp SEMICOLON  // ;
+#define colon  &kp COLON      // :
+#define excla  &kp SLASH      // !
