1ed61aed09e7912e1b226e9823440fbfe19d06a2
lunics <mail@lunics.fr>
2023-09-17 23:36:22 +0200

    rollback conf file

--- config/defines/combos.h
+++ config/defines/combos.h
@@ -35,9 +35,33 @@
       key-positions = <16 17 18>;
       bindings = <&kp ENTER>;
     };
-    cb_del_l { layers = <la_DEF la_NAV>; timeout-ms = <70>; key-positions = <21 22 23>; bindings = <&kp DEL>; };
-    cb_del_r { layers = <la_DEF la_NAV>; timeout-ms = <70>; key-positions = <26 27 28>; bindings = <&kp DEL>; };
-    cb_caps { layers = <la_DEF>; timeout-ms = <40>; key-positions = <26 27>; bindings = <&caps_word>; };
-    cb_boot_l { layers = <la_DEF>; timeout-ms = <30>; key-positions = <po_T1 24>; bindings = <&bootloader>; };
-    cb_boot_r { layers = <la_DEF>; timeout-ms = <30>; key-positions = <po_T2 25>; bindings = <&bootloader>; };
-
+    cb_del_l {
+      layers = <la_DEF la_NAV>;
+      timeout-ms = <70>;
+      key-positions = <21 22 23>;
+      bindings = <&kp DEL>;
+    };
+    cb_del_r {
+      layers = <la_DEF la_NAV>;
+      timeout-ms = <70>;
+      key-positions = <26 27 28>;
+      bindings = <&kp DEL>;
+    };
+    cb_caps {
+      layers = <la_DEF>;
+      timeout-ms = <40>;
+      key-positions = <26 27>;
+      bindings = <&caps_word>;
+    };
+    cb_boot_l {
+      layers = <la_DEF>;
+      timeout-ms = <30>;
+      key-positions = <po_T1 24>;
+      bindings = <&bootloader>;
+    };
+    cb_boot_r {
+      layers = <la_DEF>;
+      timeout-ms = <30>;
+      key-positions = <po_T2 25>;
+      bindings = <&bootloader>;
+    };
