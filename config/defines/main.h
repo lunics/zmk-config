4e8d5159512eccf17db7640fc17748e93e74b385
lunics <mail@lunics.fr>
2023-09-28 22:55:11 +0200

    done layout numbers

--- config/defines/main.h
+++ config/defines/main.h
@@ -1,11 +1,3 @@
 #include "common"
 #include "azerty"
 /* #include "qwerty" */
-
-#define l_def  0      // layout default
-#define l_num  1      // layout numbers
-#define l_sym  2
-#define l_nav  3
-#define l_med  4
-#define l_spe  5
-#define l_fun  7
