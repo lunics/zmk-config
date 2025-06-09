73a347203da326b6caaef76873ae4e1dde4a232e
lunics <git.dev@lunics.fr>
2025-06-10 00:37:55 +0200

    modify default layout to be compliant with the official ergol

--- config/layers/aurora_sweep/ergol/default.h
+++ config/layers/aurora_sweep/ergol/default.h
@@ -1,10 +1,10 @@
 #define LAYER_DEFAULT \
 /*╭─────────┬─────────┬─────────┬─────────┬─────────╮  ╭─────────┬─────────┬─────────┬─────────┬─────────╮*/\
-      q_        c_        o_         p_        w_           j_       m_        d_       squote     f_       \
+      q_        c_        o_         p_        w_           j_       m_        d_       squote     y_       \
 /*├─────────┼─────────┼─────────┼─────────┼─────────┤  ├─────────┼─────────┼─────────┼─────────┼─────────┤*/\
-   ht_symr A ht_winl S ht_ctll E ht_altl N   comma          l_    ht_ctlr R    t5        i6     ht_syml U   \
+   ht_symr A ht_winl S ht_ctll E ht_altl N     f_           l_    ht_ctlr R    t5        i6     ht_syml U   \
 /*├─────────┼─────────┼─────────┼─────────┼─────────┤  ├─────────┼─────────┼─────────┼─────────┼─────────┤*/\
-      z_     ht_fun X    minus       v_     ht_har B       dot    ht_altr H    g2        y3        k_       \
+      z_     ht_fun X    minus       v_     ht_har B       dot    ht_altr H    g2       comma      k_       \
 /*╰─────────┴─────────┴─────────┼─────────┼─────────┤  ├─────────┼─────────┼─────────┴─────────┴─────────╯*/\
                             ht_shft ESC ht_navl SPACE   ht_numr ENTER ht_sper TAB                           \
 /*                              ╰─────────┴─────────╯  ╰─────────┴─────────╯                              */\
