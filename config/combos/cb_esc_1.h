266c418b1786143bee9bb39333533643f52b8960
lunics <git.dev@lunics.fr>
2025-09-06 23:19:55 +0200

    remove cb_esc_2

--- config/combos/cb_esc_1.h
+++
@@ -1,8 +0,0 @@
-#define CB_ESC_1 \
-cb_esc_1 {\
-  layers        = <l_def l_nav>  ;\
-  timeout-ms    = <30>           ;\
-  key-positions = <cb_esc_pos_1> ;\
-  bindings      = <&kp ESC>      ;\
-  require-prior-idle-ms = <150>  ;\
-};
--- config/combos/cb_esc_2.h
+++
@@ -1,8 +0,0 @@
-#define CB_ESC_2 \
-cb_esc_2 {\
-  layers        = <l_def l_nav>  ;\
-  timeout-ms    = <30>           ;\
-  key-positions = <cb_esc_pos_2> ;\
-  bindings      = <&kp ESC>      ;\
-  require-prior-idle-ms = <150>  ;\
-};
