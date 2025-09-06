f9d1358d83eed81c0dc8762c23af9279548d5f6f
lunics <git.dev@lunics.fr>
2025-09-06 23:31:18 +0200

    add question mark in symbol layer

--- config/layers/aurora_sweep/symbols_l.h
+++ config/layers/aurora_sweep/symbols_l.h
@@ -2,7 +2,7 @@
 /*╭────────┬────────┬────────┬────────┬────────╮  ╭────────┬────────┬────────┬────────┬────────╮*/\
     grave    pipe     equal     plus    percent     trans    trans    trans    trans    trans     \
 /*├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤*/\
-    hash     star     scolon    excla    trans      trans    trans    trans    trans    trans     \
+    hash     star     q_mark    excla    trans      trans    trans    trans    trans    trans     \
 /*├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤*/\
    ampersnd  degree   undersc   tilde    trans      trans    trans    trans    trans    trans     \
 /*╰────────┴────────┴────────┼────────┼────────┤  ├────────┼────────┼────────┴────────┴────────╯*/\
