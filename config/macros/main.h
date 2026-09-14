a450a79a1fa88836c817a4eae60d342946207da4
lunics <>
2026-09-14 14:42:28 +0000

    replace macros and tap dances with zmk-helpers

--- config/macros/main.h
+++ config/macros/main.h
@@ -1,4 +1,2 @@
 #include "hm.h"
 #include "hmm.h"
-#include "macro_tap.h"
-#include "tapdance.h"
--- config/macros/tapdance.h
+++
@@ -1,8 +0,0 @@
-#define TAPDANCE(NAME, TAP_1, TAP_2) \
-NAME: NAME {\
-  compatible      = "zmk,behavior-tap-dance"  ;\
-  label           = #NAME                     ;\
-  #binding-cells  = <0>                       ;\
-  tapping-term-ms = <200>                     ;\
-  bindings        = <TAP_1>, <TAP_2>          ;\
-};
