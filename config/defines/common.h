8a29161b34cc509429994f7a9b5991847379333b
lunics <>
2026-09-15 17:21:56 +0000

    define the key position aggregates used by the hold-taps

--- config/defines/common.h
+++ config/defines/common.h
@@ -81,5 +81,10 @@
 #define bt_nxt   &bt BT_NXT
 #define bt_sel   &bt BT_SEL
 
+// zmk-helpers ships the key labels, not these aggregates
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4
+#define THUMBS LH1 LH0 RH0 RH1
+
 #define po_RIGHT KEYS_R THUMBS
 #define po_LEFT  KEYS_L THUMBS
