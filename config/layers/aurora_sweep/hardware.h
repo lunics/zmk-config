3ecda8310cb810ba11f1b97dd8a1cb55a5e853e9
lunics <>
2026-09-18 16:12:34 +0000

    hardware: put boot and reset on the hardware layer left top row

--- config/layers/aurora_sweep/hardware.h
+++ config/layers/aurora_sweep/hardware.h
@@ -1,6 +1,6 @@
 #define LAYER_HARDWARE \
 /*╭────────┬────────┬────────┬────────┬────────╮  ╭────────┬────────┬────────┬────────┬────────╮*/\
-     none     none     none     none     none        none     none     none     none     none     \
+     none     none     boot    reset     none        none     none     none     none     none     \
 /*├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤*/\
    bt_sel 0 bt_sel 1 bt_sel 2 bt_sel 3 bt_sel 4      none     none     none     none     none     \
 /*├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤*/\
